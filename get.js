4f445e0ef7777ea7bf30c9f7a3c2a08bee19778d
Rijonboss <rij7976@gmail.com>
2024-09-24 14:16:53 +0600

    Create get.js

---
+++ get.js
@@ -0,0 +1,81 @@
+## Hi there 👋
+
+<!--const { threadsData } = global.db;
+
+module.exports = function (checkAuthConfigDashboardOfThread) {
+        return {
+                isAuthenticated(req, res, next) {
+                        if (req.isAuthenticated())
+                                return next();
+                        req.flash('errors', { msg: 'You must be logged in' });
+                        res.redirect(`/login?redirect=${req.originalUrl}`);
+                },
+
+                unAuthenticated(req, res, next) {
+                        if (!req.isAuthenticated())
+                                return next();
+                        res.redirect('/');
+                },
+
+                isVeryfiUserIDFacebook(req, res, next) {
+                        if (req.user.facebookUserID)
+                                return next();
+                        req.flash('errors', { msg: 'Bạn cần phải xác thực id facebook trước khi thực hiện hành động này' });
+                        res.redirect(`/verifyfbid?redirect=${req.originalUrl}`);
+                },
+
+                isWaitVerifyAccount(req, res, next) {
+                        if (req.session.waitVerifyAccount)
+                                return next();
+                        res.redirect('/register');
+                },
+
+                async checkHasAndInThread(req, res, next) {
+                        const userID = req.user.facebookUserID;
+                        const threadID = req.method == 'POST' ? req.body.threadID : req.params.threadID;
+                        const threadData = await threadsData.get(threadID);
+                        if (!threadData) {
+                                req.flash('errors', { msg: 'Thread not found' });
+                                return res.redirect('/dashboard');
+                        }
+                        const findMember = threadData.members.find(m => m.userID == userID && m.inGroup == true);
+                        if (!findMember) {
+                                req.flash('errors', { msg: 'Bạn không ở trong nhóm chat này' });
+                                return res.redirect('/dashboard');
+                        }
+                        req.threadData = threadData;
+                        next();
+                },
+
+                async middlewareCheckAuthConfigDashboardOfThread(req, res, next) {
+                        const threadID = req.method == 'POST' ? req.body.threadID : req.params.threadID;
+                        if (checkAuthConfigDashboardOfThread(threadID, req.user.facebookUserID))
+                                return next();
+                        req.flash('errors', {
+                                msg: '[!] Chỉ quản trị viên của nhóm chat hoặc những thành viên được cho phép mới có thể chỉnh sửa dashboard'
+                        });
+                        return res.redirect('/dashboard');
+                },
+
+                async isAdmin(req, res, next) {
+                        const userID = req.user.facebookUserID;
+                        if (!global.GoatBot.config.adminBot.includes(userID)) {
+                                req.flash('errors', { msg: 'Bạn không phải là admin của bot' });
+                                return res.redirect('/dashboard');
+                        }
+                        next();
+                }
+        };
+};
+**Rijonboss/Rijonboss** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+
+Here are some ideas to get you started:
+
+- 🔭 I’m currently working on ...
+- 🌱 I’m currently learning ...
+- 👯 I’m looking to collaborate on ...
+- 🤔 I’m looking for help with ...
+- 💬 Ask me about ...
+- 📫 How to reach me: ...
+- 😄 Pronouns: ..
+-->
